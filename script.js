6246c5d518ed6c9e5db53f49043ac0ff79f8f1e2
BuildTools <unconfigured@null.spigotmc.org>
2022-10-26 22:09:06 -0600

    Added suedo code for each file type

---
+++ script.js
@@ -0,0 +1,9 @@
+//Set up onclick function for main hub channels
+
+//Set up music to play onload of quiz tabs
+
+//Create logic for talling scores (likely will be a ton of if statements, lets see if we can create a function for it)
+
+//Set up different messages linked to what score (likely will be a case statement)
+
+//Create links at bottom to get you to the top and/or get you to the hub and on the top to get you to the hub
