8ba1141f51f85ccfadaa11980386e24b3de82ab8
BuildTools <unconfigured@null.spigotmc.org>
2022-10-29 15:29:18 -0600

    Got a the correct looking show titles need to figure out how to get opacity working and then get links to open on click

--- script.js
+++ script.js
@@ -5,13 +5,18 @@
 //Create logic for talling scores
 let score=0;
 function check(id) {
-    var methods = document.getElementsById(id);
-    for (var i=0; i<methods.length; i++) {
-         if (methods[i].checked == true) {
+    var question = document.getElementsById(id);
+    for (var i=0; i<question.length; i++) {
+         if (question[i].checked == true) {
              score +=1;
              console.log(score)
             }
    }
+}
+
+function channelSurf() {
+    let opac = document.getElementsByClassName('linkImgs')
+    opac.style.opacity='0.5'
 }
 //Set up different messages linked to what score (likely will be a case statement)
 
