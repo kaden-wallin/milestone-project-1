aefc11ec6f105c69d2ac96c39ccc7c87d368bd34
BuildTools <unconfigured@null.spigotmc.org>
2022-10-29 12:58:25 -0600

    Set up quiz pages with basic forms and temporary backgrounds, set up score tally function

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-//Set up onclick function for main hub channels
+//Set up onclick functions for hub page
 
 //Set up music to play onload of quiz tabs
 
-//Create logic for talling scores (likely will be a ton of if statements, lets see if we can create a function for it)
-
+//Create logic for talling scores
+let score=0;
+function check(id) {
+    var methods = document.getElementsById(id);
+    for (var i=0; i<methods.length; i++) {
+         if (methods[i].checked == true) {
+             score +=1;
+             console.log(score)
+            }
+   }
+}
 //Set up different messages linked to what score (likely will be a case statement)
 
 //Create links at bottom to get you to the top and/or get you to the hub and on the top to get you to the hub
