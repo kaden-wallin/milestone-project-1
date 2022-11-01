b9eac3792540697fe29dec867454e67a5a77b183
BuildTools <unconfigured@null.spigotmc.org>
2022-10-31 22:30:49 -0600

    Linked the channels to the quizzes, finished the questions for the quizzes, did some more styling, organized some notes, and now trying to get the js logic in

--- script.js
+++ script.js
@@ -5,8 +5,8 @@
 //Create logic for talling scores
 let score=0;
 function check(id) {
-    var question = document.getElementsById(id);
-    for (var i=0; i<question.length; i++) {
+    var question = document.getElementById(id);
+    for (var i = 0; i < question.length; i++) {
          if (question[i].checked == true) {
              score +=1;
              console.log(score)
@@ -14,10 +14,7 @@ function check(id) {
    }
 }
 
-function channelSurf() {
-    let opac = document.getElementsByClassName('linkImgs')
-    opac.style.opacity='0.5'
-}
+
 //Set up different messages linked to what score (likely will be a case statement)
 
 //Create links at bottom to get you to the top and/or get you to the hub and on the top to get you to the hub
