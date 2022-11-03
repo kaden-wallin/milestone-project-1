42a569f47488600f7c62758b33b136ec04fa54b9
BuildTools <unconfigured@null.spigotmc.org>
2022-11-03 14:16:42 -0600

    Got the autoplay theme songs setup to play apon entering quiz page

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 //Create logic for talling scores
 function results(quiz) {
-    let score=0;
+    let score= 0;
     if (document.getElementById(quiz + 1).checked) {
         score++;
     }
@@ -20,12 +20,16 @@ function results(quiz) {
     if (document.getElementById(quiz + 5).checked) {
         score++;
     }
-    alert("Your score is: " +score + ", Thank you for playing!")
+
+    window.location.href = "../results/" + quiz + "Results.html";
+    document.getElementById('quizResult').textContent = "Your score is: " +score + ", Thank you for playing!";
+    
 }
 
 function returnToHub() {
     window.location.href = "../hub.html"
 }
+
 //Set up different messages linked to what score (likely will be a case statement)
 
 //Create links at bottom to get you to the top and/or get you to the hub and on the top to get you to the hub
