1f8a0aee9b07159bcf8e4c6431c27d75631bf58e
BuildTools <unconfigured@null.spigotmc.org>
2022-11-03 14:33:52 -0600

    Still can't figure out music, might switch back to alert message

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-//Set up onclick functions for hub page
-
-//Set up music to play onload of quiz tabs
-
 //Create logic for talling scores
-function results(quiz) {
+async function results(quiz) {
     let score= 0;
     if (document.getElementById(quiz + 1).checked) {
         score++;
@@ -22,14 +18,16 @@ function results(quiz) {
     }
 
     window.location.href = "../results/" + quiz + "Results.html";
-    document.getElementById('quizResult').textContent = "Your score is: " +score + ", Thank you for playing!";
     
+    let message = "Your score is: "  +score + ", Thank you for playing!";
+    
+    let resultmessage = document.getElementById("resultH1")
+    
+    resultmessage.innerHTML = message;
 }
-
-function returnToHub() {
-    window.location.href = "../hub.html"
-}
-
 //Set up different messages linked to what score (likely will be a case statement)
 
 //Create links at bottom to get you to the top and/or get you to the hub and on the top to get you to the hub
+function returnToHub() {
+    window.location.href = "../hub.html"
+}
