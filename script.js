0657cfb3571ecdcaff10910300ccc1a54d09ce95
BuildTools <unconfigured@null.spigotmc.org>
2022-11-04 12:01:16 -0600

    Added retry quiz button and set each results page up, still can't figure out how to display score. I hope all this effort isn't for nothing

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+const resultsContainer = document.getElementsByClassName('resultsMessage')
 //Create logic for talling scores
-async function results(quiz) {
+function results(quiz) {
     let score= 0;
     if (document.getElementById(quiz + 1).checked) {
         score++;
@@ -24,10 +25,16 @@ async function results(quiz) {
     let resultmessage = document.getElementById("resultH1")
     
     resultmessage.innerHTML = message;
+    
+    resultsContainer.innerHTML = `Your score is ${score}, Thank you for playing!`;
 }
 //Set up different messages linked to what score (likely will be a case statement)
 
 //Create links at bottom to get you to the top and/or get you to the hub and on the top to get you to the hub
 function returnToHub() {
     window.location.href = "../hub.html"
+}
+
+function retryQuiz(quiz) {
+    window.location.href ="../quizzes/" + quiz + ".html"
 }
